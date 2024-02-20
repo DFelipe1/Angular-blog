1118fa2fb81584ef3f92a0a3474a7f968ce091ef
David Felipe <felipe30.10@outlook.com>
2024-02-20 11:22:11 -0300

    feat: header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import { SmallCardComponent } from '../../components/small-card/small-card.compo
   imports: [
     BannerComponent,
     BigCardComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    HeaderComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
